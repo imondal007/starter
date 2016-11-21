47c3d7ae20abd5fcab6d804711cafc08a01f178c
Imran Mondal <hello@imranmondal.com>
2016-11-22 05:59:00 +0600

    Final Build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp        = require('gulp'),
 	browserSync = require('browser-sync').create(),
-	uglify     = require('gulp-uglify'),
-	rename     = require('gulp-rename'),
-	sass       = require('gulp-sass'),
-	plumber    = require('gulp-plumber');
+	uglify      = require('gulp-uglify'),
+	rename      = require('gulp-rename'),
+	sass        = require('gulp-sass'),
+	plumber     = require('gulp-plumber'),
+	cleanCSS    = require('gulp-clean-css');
 
 //Html
 gulp.task('html', function () {
@@ -12,41 +13,51 @@ gulp.task('html', function () {
         .pipe(gulp.dest('dist/'));
 });
 
+// Static Server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: "./"
+    });
+});
+
 //Styles
 gulp.task('styles', function () {
 	gulp.src('scss/*.scss')
 		.pipe(plumber())
 		.pipe(sass({outputStyle: 'expanded'}))
 		.pipe(gulp.dest('css/'))
-		.pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('dist/css/'));
+		.pipe(browserSync.stream());
+});
+
+//CSS
+gulp.task('css', function() {
+  	gulp.src('css/*.css')
+  		.pipe(gulp.dest('dist/css'))
+    	.pipe(cleanCSS({compatibility: 'ie8'}))
+    	.pipe(rename({suffix:'.min'}))
+    	.pipe(gulp.dest('dist/css'));
 });
 
 //Uglify
 gulp.task('scripts', function () {
 	gulp.src('js/*.js')
 		.pipe(plumber())
+		.pipe(gulp.dest('dist/js'))
 		.pipe(uglify())
+		.pipe(rename({suffix:'.min'}))
 		.pipe(gulp.dest('dist/js'));
-});
-
-// Static Server + watching scss/html files
-gulp.task('serve', function() {
-    browserSync.init({
-        server: './'
-    });
 });
 
 //Watch Task
 gulp.task('watch', function() {
 	gulp.watch('js/*.js', ['scripts']);
+	gulp.watch('*.css', ['css']);
 	gulp.watch('*.html', ['html']);
-	gulp.watch('scss/*.scss', ['styles']);
-	gulp.watch('*.html').on('change', browserSync.reload);
-	gulp.watch('css/*.css').on('change', browserSync.reload);
+	gulp.watch("scss/*.scss", ['styles']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 })
 
 
 
 // Default Task
-gulp.task('default', ['styles', 'html', 'scripts', 'serve', 'watch']);
+gulp.task('default', ['styles', 'css', 'html', 'scripts', 'serve', 'watch']);
