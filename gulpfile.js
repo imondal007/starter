beaf4851b8b4050d64ac4219a34613d69a876ddb
Imran Mondal <hello@imranmondal.com>
2017-03-11 03:23:34 +0600

    Updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,83 @@
-var gulp        = require('gulp'),
-	browserSync = require('browser-sync').create(),
-	uglify      = require('gulp-uglify'),
-	rename      = require('gulp-rename'),
-	sass        = require('gulp-sass'),
-	plumber     = require('gulp-plumber'),
-	cleanCSS    = require('gulp-clean-css'),
-	imagemin    = require('gulp-imagemin');
+var gulp = require('gulp'),
+  browserSync = require('browser-sync').create(),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  plumber = require('gulp-plumber'),
+  cleanCSS = require('gulp-clean-css'),
+  imagemin = require('gulp-imagemin'),
+  fs = require('fs'); // Node module for file system
 
-
-// Static Server
+// browserSync Server
 gulp.task('serve', function() {
-    browserSync.init({
-        server: "./"
-    });
+  browserSync.init({
+    server: "./"
+  });
 });
 
 //Styles
-gulp.task('styles', function () {
-	gulp.src('scss/*.scss')
-		.pipe(plumber())
-		.pipe(sass({outputStyle: 'expanded'}))
-		.pipe(gulp.dest('css/'))
-		.pipe(browserSync.stream());
+gulp.task('styles', function() {
+  gulp.src('scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('css/'))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
 });
 
-//Watch Task
-gulp.task('watch', function() {
-	gulp.watch("scss/*.scss", ['styles']);
-    gulp.watch("css/*.css").on('change', browserSync.reload);
-    gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("js/*.js").on('change', browserSync.reload);
-})
-
-
-// Default Task
-gulp.task('default', ['styles', 'serve', 'watch']);
-
-
-//////////////////////////////////////////////////////////
-///                      Build Task                    ///
-//////////////////////////////////////////////////////////
-
-// Build Server
-gulp.task('build-serve', function() {
-    browserSync.init({
-        server: "./dist"
-    });
-});
-
-//CSS
-gulp.task('css', function() {
-  	gulp.src('css/*.css')
-  		.pipe(gulp.dest('dist/css'))
-    	.pipe(cleanCSS({compatibility: 'ie8'}))
-    	.pipe(rename({suffix:'.min'}))
-    	.pipe(gulp.dest('dist/css'));
-});
-
-//Html
-gulp.task('html', function () {
-	gulp.src('*.html')
-		.pipe(plumber())
-        .pipe(gulp.dest('dist/'));
+//Script
+gulp.task('script', function() {
+  gulp.src(['js/*.js', '!js/*.min.js'])
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('js/'));
 });
 
 //Images
 gulp.task('image', () =>
-    gulp.src('img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
+  gulp.src('img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('img/'))
 );
 
-//Uglify
-gulp.task('scripts', function () {
-	gulp.src('js/*.js')
-		.pipe(plumber())
-		.pipe(gulp.dest('dist/js'))
-		.pipe(uglify())
-		.pipe(rename({suffix:'.min'}))
-		.pipe(gulp.dest('dist/js'));
+
+//Watch Task
+gulp.task('watch', function() {
+  gulp.watch("scss/*.scss", ['styles']);
+  gulp.watch("img/*", ['image']);
+  gulp.watch(['js/*.js', '!js/*.min.js'], ['script']);
+  gulp.watch("css/*.min.css").on('change', browserSync.reload);
+  gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("js/*min.js").on('change', browserSync.reload);
+})
+
+
+gulp.task('copy', function() {
+  if (fs.existsSync('./src')) {
+    // Do Nothing if src directory available
+  } else {
+    // If src directory not available run the task
+
+    // Copy Bootstrap
+    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+      .pipe(gulp.dest('src/bootstrap'));
+    // Copy Font-awesome
+    gulp.src([
+        'node_modules/font-awesome/**/*',
+        '!node_modules/font-awesome/less{,/**}',
+        '!node_modules/font-awesome/scss{,/**}',
+        '!node_modules/font-awesome/**/*.map',
+        '!node_modules/font-awesome/.npmignore',
+        '!node_modules/font-awesome/*.txt',
+        '!node_modules/font-awesome/*.md',
+        '!node_modules/font-awesome/*.json'
+      ])
+      .pipe(gulp.dest('src/font-awesome'));
+  }
 });
 
-//Build Task
-gulp.task('build', ['css', 'html', 'scripts', 'image', 'build-serve']);
+// Default Task
+gulp.task('default', ['styles', 'script', 'copy', 'serve', 'watch']);
